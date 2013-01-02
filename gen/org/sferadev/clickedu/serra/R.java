7f0e59e6c48be5c614249743559688267f26683f
SferaDev <sferarc@gmail.com>
2013-01-02 17:01:09 +0100

    Mass Rewrite

--- gen/org/sferadev/clickedu/serra/R.java
+++ gen/org/sferadev/clickedu/serra/R.java
@@ -11,16 +11,20 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070006;
-        public static final int tab1=0x7f070000;
-        public static final int tab2=0x7f070002;
-        public static final int tab3=0x7f070004;
-        public static final int textView1=0x7f070001;
-        public static final int textView2=0x7f070005;
-        public static final int webview=0x7f070003;
+        public static final int donate=0x7f070008;
+        public static final int item1=0x7f070009;
+        public static final int linearlayout=0x7f070003;
+        public static final int tab2=0x7f070000;
+        public static final int tab3=0x7f070002;
+        public static final int textView1=0x7f070004;
+        public static final int textView2=0x7f070007;
+        public static final int textView3=0x7f070005;
+        public static final int textView4=0x7f070006;
+        public static final int webview=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -29,8 +33,12 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int about_title=0x7f040002;
         public static final int appname=0x7f040000;
-        public static final int menu_settings=0x7f040001;
+        public static final int copyright=0x7f040005;
+        public static final int donate=0x7f040001;
+        public static final int text_about=0x7f040004;
+        public static final int thanks=0x7f040003;
     }
     public static final class style {
         /** 
--- src/org/sferadev/clickedu/serra/SerraActivity.java
+++ src/org/sferadev/clickedu/serra/SerraActivity.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Field;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.TabActivity;
+
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.ViewConfiguration;
 import android.webkit.WebSettings;
@@ -13,42 +15,53 @@ import android.webkit.WebViewClient;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends TabActivity {
+public class SerraActivity extends TabActivity {
+
+	WebView webview2;
+	WebSettings websettings2;
+	TabHost host;
+	TabSpec spec2;
+	TabSpec spec3;
+
+	public void onBackPressed (){
+
+	    if (webview2.isFocused()) {
+	            webview2.loadUrl("http://clickedu.coleserra.net/alumnes/index.php");      
+	            webview2.setWebViewClient(new InsideWebViewClient2());
+	            websettings2 = webview2.getSettings();
+	            websettings2.setJavaScriptEnabled(true);
+	            websettings2.setBuiltInZoomControls(true);
+	    }
+	    
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		TabHost host = (TabHost)findViewById(android.R.id.tabhost);
+		host = (TabHost)findViewById(android.R.id.tabhost);
 		
-		TabSpec spec2 = host.newTabSpec("tab2");
+		spec2 = host.newTabSpec("tab2");
         spec2.setContent(R.id.tab2);
         spec2.setIndicator("Clickedu");
         host.addTab(spec2);
-        WebView webview2 = (WebView) this.findViewById(R.id.webview);
-        WebSettings websettings2 = webview2.getSettings();
+        webview2 = (WebView) this.findViewById(R.id.webview);
+        websettings2 = webview2.getSettings();
         websettings2.setJavaScriptEnabled(true);
         websettings2.setBuiltInZoomControls(true);
         webview2.loadUrl("http://clickedu.coleserra.net/m/m_user.php");
         webview2.setWebViewClient(new InsideWebViewClient2());
+        
 		
-        TabSpec spec1 = host.newTabSpec("tab1");
-        spec1.setContent(R.id.tab1);
-        spec1.setIndicator("SferaDev");
-        host.addTab(spec1);
         
-        
-        
-        TabSpec spec3 = host.newTabSpec("tab3");
+        spec3 = host.newTabSpec("tab3");
         spec3.setContent(R.id.tab3);
         spec3.setIndicator("About");
         host.addTab(spec3);
         
-        
-        
-	}
+        }
+	
 
- 
 	private void getOverflowMenu() {
 		
 		try {
@@ -63,15 +76,20 @@ public class MainActivity extends TabActivity {
 		}
 	}
 	
+
+
 	
     private class InsideWebViewClient2 extends WebViewClient {
     	public boolean shouldOverrideUrlLoading(WebView view, String url1) {
     		view.loadUrl(url1);
     		return true;
     	}
+    	
     }
     
-
+   
+	
+    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
